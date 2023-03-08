78af8ca4878c50ce101b66c64ffda7d655698322
Anna Adamik <quenthel5@gmail.com>
2023-03-08 11:46:44 +0100

    update

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
+
     public static void main(String[] args) {
+
         System.out.println("Hello, Ania");
 
         boolean isAlien = false;
@@ -17,10 +19,18 @@ public class Hello {
                 System.out.println("Greater than second top score and less than 100");
             }
 
-
-
             if ((topScore > 90) || (secondTopScore <= 90)){
                 System.out.println("Either or both of the conditions are true");
+            }
+
+            int newValue = 50;
+            if (newValue == 50) {
+                System.out.println("This is true");
+            }
+
+            boolean isCar = false;
+            if (!isCar) {
+                System.out.println("This is not supposed to happen");
             }
         }
     }
